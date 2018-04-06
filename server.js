c57bc712a51ad10a2af655882392c46a18f7d6a6
Jorge R <jorgerodriguez434@gmail.com>
2018-04-05 23:55:08 -0500

    try to build Travis CI

--- server.js
+++ server.js
@@ -8,80 +8,7 @@ mongoose.Promise = global.Promise;
 
 const morgan = require('morgan');
 
-//const router = require('./router');
-
-
-
-
-const { PlayList } = require('./models');
-
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
-
-app.get('/api/playlist/', (req, res) => {
-
-      console.log('making a GET request');
-      PlayList.find()
-      .then(playlist => res.status(200).json(playlist))
-      .catch(err => {
-
-          console.error(err);
-          res.status(500).json({error: `${err}`})
-
-      })
-});
-
-app.post('/api/playlist/', jsonParser, (req, res) => {
-
-    console.log('making a POST request');
-    PlayList.create({
-
-                  local_id: req.body.local_id, //it is body
-                  song: req.body.song,
-                  artist: req.body.artist
-
-    })
-    .then(item => res.status(201).json(item));
-
-});
-
-app.delete('/api/playlist/:id', (req, res) => {
-
-    console.log('making a DELETE request');
-    //need to target a specific item
-    PlayList.findByIdAndRemove(req.params.id)
-    .then(item => {
-        console.log(item);
-        res.json(item);
-        res.status(200).end();
-    });
-
-});
-
-app.put('/api/playlist/:id', jsonParser, (req, res) => {
-
-    console.log('making a PUT request')
-    PlayList.findByIdAndUpdate(req.params.id, 
-                        { 
-                          $set:
-                          {
-                            song: `${req.body.song}`,
-                            artist: `${req.body.artist}`
-                          }
-                        }, 
-                        { new: true } )
-    .then(item => {
-
-        console.log(item);
-        res.json(item);
-        res.status(200).end();
-
-    });
-
-});
-
-
-
+const router = require('./router');
 const { DATABASE_URL, PORT } = require('./config');
 
 const cors = require('cors');
@@ -94,9 +21,10 @@ app.use(function(req, res, next) {
 
 app.use(morgan('common'));
 app.use(express.static('public'));
-//app.use('/api/playlist', router)
+app.use('/api/playlist', router)
  
 let server;
+
 // this function connects to our database, then starts the server
 function runServer(databaseUrl, port = PORT) {
   return new Promise((resolve, reject) => {
@@ -105,7 +33,7 @@ function runServer(databaseUrl, port = PORT) {
         return reject(err);
       }
       server = app.listen(port, () => {
-        console.log(`Your app is listening on port ${port} --April 5th, 2018`);
+        console.log(`Your app is listening on port ${port} --March 21st, 2018`);
         resolve();
       })
         .on('error', err => {
