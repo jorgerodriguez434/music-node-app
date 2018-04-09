3244d759405b31817f01b7e01506ff4ce1c1d726
Jorge R <jorgerodriguez434@gmail.com>
2018-04-09 02:58:52 -0500

    go back to playlist in router

--- router.js
+++ router.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
 
       console.log('making a GET request');
       PlayList.find()
-      .then(data => res.status(200).json(data.playlist));
+      .then(playlist => res.status(200).json(playlist));
 
 });
 
