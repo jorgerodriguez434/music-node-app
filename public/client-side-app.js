ecb89950d50a1b9c8a2960bdab8a2f5e3771141a
Jorge R <jorgerodriguez434@gmail.com>
2018-04-10 11:02:22 -0500

    add post button

--- public/client-side-app.js
+++ public/client-side-app.js
@@ -1,25 +1,55 @@
 console.log("hi");
+const PLAYLIST_API_ENDPOINT = 'https://lychee-shortcake-58019.herokuapp.com/api/playlist';
+const PLAYLIST = $('.playlist');
 
 $('.get-button').on('click', event => {
+    
     event.preventDefault();
-    $.getJSON('https://lychee-shortcake-58019.herokuapp.com/api/playlist')
+    $.getJSON(PLAYLIST_API_ENDPOINT)
     .then(data => {
-    console.log(data);
-	 	data.map(item => {
+      
+          console.log(data);
+          if(data.length === 0){
+            console.log("There are no songs!");
+            PLAYLIST.text("There are no songs!");
+          } 
+          PLAYLIST.empty()
+	 	      data.map(item => {
 		
-   		$('body').append(
+   	            PLAYLIST.append(
 
-   					`
-   					   <h4> ${item.song}  </h4>
-   					   <p> -- ${item.artist} </p>
+   					      ` 
+   					           <h4> ${item.song}  </h4>
+   					          <p> -- ${item.artist} </p>
 
-  					`
-   			);
+  					      `
+   			        );
 
-  	 });  
+  	      });  
 
-   }).catch(err => console.log(err)); 
+   })
+   .catch(err => console.log(err)); 
   
-   $('.playlist').hide();
+   PLAYLIST.addClass('purple');
     
 });
+
+$('.post-button').on('click', event => {
+  
+    event.preventDefault();
+    $.post(PLAYLIST_API_ENDPOINT, JSON.stringify({
+      
+            local_id: "010",
+		        song: "Baby One More Time",
+		        genre: "Pop",
+		        artist: "Britney Spears"
+      
+    }))
+    .then(data => {
+      
+            console.log(data);
+      
+    });
+    
+});
+
