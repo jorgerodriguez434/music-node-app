97722820b7a371de0a6e57aa7a82fa1345fc484b
Jorge R <jorgerodriguez434@gmail.com>
2018-04-20 09:32:18 -0500

    work with single song

--- public/client-side-app.js
+++ public/client-side-app.js
@@ -13,6 +13,7 @@ function fetch () {
 
 function render(data) {
 
+  PLAYLIST.empty();
   console.log(data);
   if(data.length === 0){
             console.log("There are no songs!");
@@ -24,11 +25,11 @@ function render(data) {
    					      ` 
                     <div data-id="${item._id}" class="current-song">
                       
-                        <h4>  ${item.song} </h4> 
-                        <p id="Z"> ${item.artist} </p>
-                        <p id ="Y"> ${item.genre} </p>
+                        <h4 class="song">  ${item.song} </h4> 
+                        <p class="artist"> ${item.artist} </p>
+                        <p class="genre"> ${item.genre} </p>
                        <button class="button update-button">UPDATE SONG</button></a>  
-                      <!-- <button class="button remove-button">REMOVE</button></a> -->
+                      <button class="button remove-button">REMOVE</button></a>
                     
                    </div>
                    <hr/>
@@ -39,20 +40,7 @@ function render(data) {
             });  
 }
 
-function asyncJS (cb) {
-
-   return new Promise(function(resolve, reject){
-     return setTimeout(function() {
-      resolve('success!');
-    }, 3000)
-   });
-
-}
-
-
-//remove
-/*
-PLAYLIST.on('click', 'div button', function(event) {
+PLAYLIST.on('click', '.remove-button', function(event) {
 
     console.log("Making a DELETE request");
     event.preventDefault();
@@ -69,56 +57,34 @@ PLAYLIST.on('click', 'div button', function(event) {
         }
     });
 
-}); */
+}); 
 
-
-/* 
-  1. When I click on an update button, I want to get current id
-  2. I want to take that id and apply it globally
-  3. It has to be asynchronous 
-*/
-//update
 function update() {
-  const currentSongDiv = $('.current-song');
-  const song = currentSongDiv.find('h4').text();
-  const artist = currentSongDiv.find('#Z').text();
-  const genre = currentSongDiv.find('#Y').text();
-  //$('div').find('span').hide();
+
+  
+  const song = $('.song').text();
+  const artist = $('.artist').text();
+  const genre = $('.genre').text();
+  $('.remove-button').hide();
  
-  currentSongDiv.append(`
-    <form>
-    <input class="input-song" type="text" value="${song}"> 
-    <input class="input-artist" type="text" value="${artist}">
-    <input class="input-genre" type="text" value="${genre}"> 
+  $('.current-song').append(`
+    <form id="F" class="song-form">
+      <input class="input-song" type="text" value="${song}"> 
+      <input class="input-artist" type="text" value="${artist}">
+      <input class="input-genre" type="text" value="${genre}"> 
     </form>
 `);
   
-  currentSongDiv.find('button').text('CONFIRM');  
+  $('.update-button').text('CONFIRM');  
 }
 
-/* 
-    .then(function(response){
-     $('div').find('h1').text(song);
-     $('div').find('h2').text(artist);
-     $('div').find('h3').text(genre);
-  
-     that.off();
-     that.click(update);
-     that.text('update');
-     $('div').find('span').show();
-     $('div').find('form').remove();
-  });
-*/
 function confirm() {
-
-  const currentSongDiv = $('.current-song');
-  const userSong = currentSongDiv.find('.input-song').val();
-  const userArtist = currentSongDiv.find('.input-artist').val();
-  const userGenre = currentSongDiv.find('.input-genre').val();
-  //const data = {song, artist, genre};
-  //const that = $(this);
-  currentSongDiv.remove();
-  //then render updated song
+  const currentDiv = $(this).parent();
+  const userSong = $('.input-song').val();
+  const userArtist = $('.input-artist').val();
+  const userGenre = $('.input-genre').val();
+  const that = $(this);
+  console.log("Making a PUT request");
   $.ajax({
         url: `http://localhost:8080/api/playlist/${id}`,
         method: 'PUT',
@@ -129,32 +95,34 @@ function confirm() {
             genre: userGenre
         },
         success: function(data) {
+            console.log(id)
             console.log(data);
-           //continue here
-           //render updated song
-           currentSongDiv.find('h4').text();
-           currentSongDiv.find('#Z').text();
-           currentSongDiv.find('#Y').text();
-
+            console.log('SUCCESS!');
+            $('.song').text(userSong);
+            $('.artist').text(userArtist);
+            $('.genre').text(userGenre);
+            $('.update-button').text('UPDATE SONG');
+            $('.song-form').remove();
+            $('.remove-button').show();
+            that.off();
+            //continue here
         },
         error: function(err) {
           console.log(err);
         }
     });
 
-}//confirm
+}
 
-
-let id="no entry";
-PLAYLIST.on('click', 'div button', function(event) {
+let id= "no entry"
+PLAYLIST.on('click', '.update-button', function(event) {
 
     console.log("Update button has been clicked");
     id = $(this).parent().attr('data-id');
     const self = this;
     console.log(self);
     console.log(`${id} :This should be the current ID`);
-    //need to continue from here
-    console.log("Making a PUT request");
+    //continue from here
     update();
     $(self).off();
      console.log(self);
@@ -179,7 +147,7 @@ $('.add-song-button').on('click', function (event) {
         },
         success: function(data) {
             console.log(data);
-            $('.js-insert').text('You have successfully added a song!');
+            $('.added-song-message').show();
         },
         error: function(err) {
           console.log(err);
@@ -187,33 +155,5 @@ $('.add-song-button').on('click', function (event) {
     });
 
 });
-/*
-$('.confirm-update-button').on('click', function(event) {
-
-    event.preventDefault();
-    const userSong = $("#song-id").val();
-    const userArtist = $("#artist-id").val();
-    const userGenre = $("#genre-id").val();
-
-    console.log("Making a PUT request");
-    $.ajax({
-        url: `http://localhost:8080/api/playlist/${id}`,
-        contentType: 'application/json',
-        method: 'PUT',
-        data: {
-
-            song: userSong,
-            artist: userArtist,
-            genre: userGenre
-        },
-        success: function(data) {
-            console.log(data);
-        },
-        error: function(err) {
-          console.log(err);
-        }
-    });
-}); */
-
 
 fetch();
