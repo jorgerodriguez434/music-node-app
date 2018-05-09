82e03781ad5f179a26c70148972000bfa95747eb
Jorge R <jorgerodriguez434@gmail.com>
2018-05-09 14:56:42 -0500

    sub heroku url

--- public/client-side-app.js
+++ public/client-side-app.js
@@ -1,5 +1,5 @@
 console.log("hi, from client-side-app!");
-const PLAYLIST_API_ENDPOINT = 'http://localhost:8080/api/playlist';
+const PLAYLIST_API_ENDPOINT = 'https://lychee-shortcake-58019.herokuapp.com/api/playlist';
 const YOUTUBE_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
 const PLAYLIST = $('.playlist');
 
